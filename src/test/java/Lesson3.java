9e5ba50cf4e2bfb53e35b3105e95902e3aa15f5e
AntonK <antonkorodenko@gmail.com>
2022-01-30 22:48:10 +0300

    добавили задание 3 про имена

---
+++ src/test/java/Lesson3.java
@@ -0,0 +1,9 @@
+public class Lesson3 {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println("Robert");
+        System.out.println("Stannis");
+        System.out.println("Renly");
+        // END
+    }
+}
