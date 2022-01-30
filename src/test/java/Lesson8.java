f5222fcd49b70b52c95206e584b710dfc7c0b672
AntonK <antonkorodenko@gmail.com>
2022-01-30 23:35:00 +0300

    добавили задание 8 про коммутативные операции

---
+++ src/test/java/Lesson8.java
@@ -0,0 +1,8 @@
+public class Lesson8 {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println(3*5);
+        System.out.println((-8)/(-4));
+        // END
+    }
+}
