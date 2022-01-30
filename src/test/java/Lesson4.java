73cac3689a77bb95f4b8e4daac96c9bc2177de92
AntonK <antonkorodenko@gmail.com>
2022-01-30 23:21:12 +0300

    добавили задание 4 про What Is Dead May Never Die

---
+++ src/test/java/Lesson4.java
@@ -0,0 +1,7 @@
+public class Lesson4 {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println("What Is Dead May Never Die");
+        // END
+    }
+}
