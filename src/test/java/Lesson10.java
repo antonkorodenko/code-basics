95e7ff67cbd079d7f0411f7234b51b96b7494713
AntonK <antonkorodenko@gmail.com>
2022-01-31 17:19:08 +0300

    добавили задание 11 добавили бибилотеку Junit

--- src/test/java/Lesson10.java
+++ src/test/java/Lesson10.java
@@ -1,7 +1,15 @@
+import org.junit.jupiter.api.Test;
+
 public class Lesson10 {
-    public static void main(String[] args) {
-        // BEGIN (write your solution here)
+
+    @Test
+    void test(){
         System.out.println((70 * (3 + 4)) / (8 + 2));
-        // END
+        System.out.println("Мы внутри теста");
+    }
+
+    @Test
+    void test1(){
+        System.out.println("Мы внутри теста 1");
     }
 }
---
+++ src/test/java/Lesson11.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson11 {
+    @Test
+    void test() {
+        System.out.println(0.39 * 0.22);
+    }
+}
