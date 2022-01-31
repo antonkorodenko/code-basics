f05c5bd730ef597356423590ae77cb4463c8d28f
AntonK <antonkorodenko@gmail.com>
2022-01-31 20:02:04 +0300

    добавили задание 16 что такое переменная

---
+++ src/test/java/Lesson16.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson16 {
+
+    @Test
+    void test(){
+        var motto = "What Is Dead May Never Die!";
+        System.out.println(motto);
+    }
+}
