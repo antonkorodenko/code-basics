bd15b4f1efa880c61dc9a7e966bb3b6482503915
AntonK <antonkorodenko@gmail.com>
2022-01-31 21:06:27 +0300

    добавили задание 20 выражения в определениях

---
+++ src/test/java/Lesson20.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson20 {
+
+    @Test
+    void test(){
+        var eurosCount = 100;
+
+        // BEGIN
+        var dollarsCount = eurosCount * 1.25;
+        System.out.print(dollarsCount);
+        System.out.print("\n");
+
+        var rublesCount = dollarsCount * 60;
+        System.out.print(rublesCount);
+        System.out.print("\n");
+    }
+}
