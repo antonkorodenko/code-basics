c9cf202a57228d4e079a0eadee2bb532ef9b7ec7
AntonK <antonkorodenko@gmail.com>
2022-01-31 20:26:41 +0300

    добавили задание 18 выбор имени переменной

---
+++ src/test/java/Lesson18.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson18 {
+
+    @Test
+    void test(){
+        var firstnumber = 1.10;
+        var secondnumber = -100;
+        System.out.println(firstnumber * secondnumber);
+    }
+}
