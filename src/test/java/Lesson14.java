b7971ea9b4bae5662ecb836c14b10d49f6e9bc9e
AntonK <antonkorodenko@gmail.com>
2022-01-31 19:43:52 +0300

    добавили задание 14 экранирующие последовательности

---
+++ src/test/java/Lesson14.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson14 {
+
+    @Test
+    void test(){
+        System.out.println("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".");
+    }
+}
