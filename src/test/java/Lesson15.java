bcae06e763d1bf5d92fadcb41b5ada3892d6fc62
AntonK <antonkorodenko@gmail.com>
2022-01-31 19:54:50 +0300

    добавили задание 15 конкатенация

---
+++ src/test/java/Lesson15.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson15 {
+
+    @Test
+    void test(){
+        System.out.println("Winter " + "came " + "for " + "the " + "House " + "of " + "Frey.");
+    }
+}
