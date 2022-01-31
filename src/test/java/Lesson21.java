53030a68addee9c11aa97ef3563c03d57d35b282
AntonK <antonkorodenko@gmail.com>
2022-01-31 21:44:06 +0300

    добавили задание 21 переменные и конкатенация

---
+++ src/test/java/Lesson21.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson21 {
+
+    @Test
+    void test(){
+        var info = "We couldn't verify you mother's maiden name.";
+        var intro = "Here is important information about your account security.";
+
+        var firstName = "Joffrey";
+        var greeting = "Hello";
+
+        System.out.println(greeting + ", " + firstName + "!");
+        System.out.println(intro + "\n" + info);
+    }
+}
