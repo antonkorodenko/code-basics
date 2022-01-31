c54c8fcf95f75915c11dbd5e47f652c2b3c9d214
AntonK <antonkorodenko@gmail.com>
2022-01-31 20:31:54 +0300

    добавили задание 19 ошибки при работе с переменными

---
+++ src/test/java/Lesson19.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson19 {
+
+    @Test
+    void test(){
+        var family = "Targaryen";
+        var pet = "Dragon";
+
+
+        System.out.print(family);
+        System.out.print(" and ");
+        System.out.print(pet);
+    }
+}
