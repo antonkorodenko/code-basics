dd2b5fed01d8038f3e79643abf8945d34f5ade5f
AntonK <antonkorodenko@gmail.com>
2022-01-31 16:07:25 +0300

    добавили задание 10 про приоритет операций

---
+++ src/test/java/Lesson10.java
@@ -0,0 +1,7 @@
+public class Lesson10 {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println((70 * (3 + 4)) / (8 + 2));
+        // END
+    }
+}
