184f9b8c877707781766dc7805a3362f1aae64a7
AntonK <antonkorodenko@gmail.com>
2022-01-31 19:09:15 +0300

    добавили задание 13 кавычки

---
+++ src/test/java/Lesson13.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson13 {
+
+    @Test
+    void test(){
+        System.out.println("\"Khal Drogo's favorite word is \"athjahakar\"\"");
+    }
+}
