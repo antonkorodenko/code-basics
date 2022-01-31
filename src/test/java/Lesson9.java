d0de895345c5786207bdaab1572010935bfa1a47
AntonK <antonkorodenko@gmail.com>
2022-01-31 16:01:42 +0300

    добавили задание 9 про композицию операций

---
+++ src/test/java/Lesson9.java
@@ -0,0 +1,7 @@
+public class Lesson9 {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println((8 / 2 + 5) - (-3 / 2));
+        // END
+    }
+}
