859912457aabac025a543676d53e5916d97bfda1
AntonK <antonkorodenko@gmail.com>
2022-01-31 20:10:08 +0300

    добавили задание 17 изменение переменной

---
+++ src/test/java/Lesson17.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson17 {
+
+    @Test
+    void test(){
+        var name = "Brienna";
+
+        name = "anneirB";
+        System.out.println(name);
+    }
+}
