eff1b940ab0aceb46de9904cc5ab87767b001484
AntonK <antonkorodenko@gmail.com>
2022-02-01 15:58:35 +0300

    добавили задание 22 именование переменных

---
+++ src/test/java/Lesson22.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson22 {
+
+    @Test
+    void test(){
+
+        var firstNumber = 11;
+        var secondNumber = -100;
+
+        System.out.println(firstNumber*secondNumber);
+    }
+}
