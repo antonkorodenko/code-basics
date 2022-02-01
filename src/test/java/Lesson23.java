d8ce956906143b9ae4356dc2e6d11b05e7b8d8fd
AntonK <antonkorodenko@gmail.com>
2022-02-01 18:01:08 +0300

    добавили задание 23 магические числа

---
+++ src/test/java/Lesson23.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson23 {
+
+    @Test
+    void test(){
+        var king = "King Balon the 6th";
+
+        var numberOfKing = 6;
+        var numberOfRooms = 17;
+        System.out.println(king + " has " + (numberOfKing * numberOfRooms) + " rooms.");
+    }
+}
