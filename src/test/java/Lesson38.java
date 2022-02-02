0fb4cd2bf98dd40fb38a896795f491ea35c326bb
AntonK <antonkorodenko@gmail.com>
2022-02-02 20:28:05 +0300

    добавили задание 38 какие бывают методы

---
+++ src/test/java/Lesson38.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson38 {
+
+    @Test
+    void test(){
+
+        var emoji = "-(";
+        // BEGIN
+        var happyEmoji = ":" + emoji.replace('(', ')');
+        System.out.println(happyEmoji);
+    }
+}
