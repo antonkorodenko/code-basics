574cf3efad61e950d4364baaca06c6a36d707b9a
AntonK <antonkorodenko@gmail.com>
2022-02-02 19:23:37 +0300

    добавили задание 34 цепочки вызовов методов

---
+++ src/test/java/Lesson34.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson34 {
+
+    @Test
+    void test(){
+
+        var name = "hexlet";
+
+        var capitalized = name.substring(0, 1).toUpperCase()
+                + name.substring(1).toLowerCase();
+        System.out.println(capitalized);
+    }
+}
