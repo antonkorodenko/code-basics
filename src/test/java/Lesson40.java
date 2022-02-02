66947ff5284a7ddf65f87b2bc33d0d7a3fbf6569
AntonK <antonkorodenko@gmail.com>
2022-02-02 21:42:29 +0300

    добавили задание 40 возврат значений

---
+++ src/test/java/Lesson40.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Lesson40 {
+    // BEGIN (write your solution here)
+    public static int getCurrentYear() {
+        var date = LocalDate.now();
+        return Integer.parseInt(date.toString().substring(0, 4));
+    }
+    // END
+}
