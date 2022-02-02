548c53f172c6ca4ca615fa077810a821e60a9078
AntonK <antonkorodenko@gmail.com>
2022-02-02 18:16:26 +0300

    добавили задание 28 значение null

---
+++ src/test/java/Lesson28.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson28 {
+
+    @Test
+    void test(){
+
+        String greeting;
+
+        greeting = "Hello Hexlet!";
+        System.out.println(greeting);
+    }
+}
