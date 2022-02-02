6152fc53e7b8f0bf964e790947e952b63838ebd5
AntonK <antonkorodenko@gmail.com>
2022-02-02 16:26:00 +0300

    добавили задание 26 явная типизация

---
+++ src/test/java/Lesson26.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson26 {
+
+    @Test
+    void test(){
+
+        String string = "One more time";
+        System.out.println(string);
+    }
+}
