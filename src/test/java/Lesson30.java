5f8db1991da1577d14411f04360112b2bae5f0a6
AntonK <antonkorodenko@gmail.com>
2022-02-02 18:20:12 +0300

    добавили задание 30 методы и их вызов

---
+++ src/test/java/Lesson30.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+public class Lesson30 {
+
+    @Test
+    void test(){
+
+        var company1 = "Apple";
+        var company2 = "Samsung";
+
+        var company1Length = company1.length();
+        var company2Length = company2.length();
+
+        System.out.println(company1Length + company2Length);
+    }
+}
