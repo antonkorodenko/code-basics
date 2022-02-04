b1a20003965d80360fd8061909773a15195f0308
AntonK <antonkorodenko@gmail.com>
2022-02-04 20:40:03 +0300

    добавили задание 47 отрицание

--- src/test/java/logic/Lesson47.java
+++ src/test/java/logic/Lesson47.java
@@ -2,10 +2,17 @@ package logic;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 public class Lesson47 {
 
     @Test
-    void test(){
+    void test() {
+        System.out.println(notToday("2012-11-25"));
+    }
 
+    public boolean notToday(String dateAsString) {
+        var date = LocalDate.now();
+        return !dateAsString.equals(date.toString());
     }
 }
