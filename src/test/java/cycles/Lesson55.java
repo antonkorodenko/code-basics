d76dba4c6b05fc0414c6c39ec943e878b373ad79
AntonK <antonkorodenko@gmail.com>
2022-02-04 23:13:32 +0300

    добавили задание 55 агрегация данных(строки)

--- src/test/java/cycles/Lesson55.java
+++ src/test/java/cycles/Lesson55.java
@@ -6,6 +6,20 @@ public class Lesson55 {
 
     @Test
     void test(){
+        System.out.println(joinNumbersFromRange(1, 1));
 
+    }
+    public static String joinNumbersFromRange(int start, int finish) {
+        // BEGIN (write your solution here)
+        var i = start;
+        var result = "";
+
+        while (i <= finish) {
+            result = result + i;
+            i = i + 1;
+        }
+
+        return result;
+        // END
     }
 }
