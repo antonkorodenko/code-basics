7397e9bcf546aa43a039daeda37bed79360b1144
AntonK <antonkorodenko@gmail.com>
2022-02-04 21:15:31 +0300

    добавили задание 51 тернарный оператор

--- src/test/java/conditionalconstructions/Lesson51.java
+++ src/test/java/conditionalconstructions/Lesson51.java
@@ -1,11 +1,15 @@
 package conditionalconstructions;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
 public class Lesson51 {
 
     @Test
     void test(){
-
+        System.out.println(convertString("hello"));
+    }
+    public static String convertString(String str) {
+        return Character.isUpperCase(str.charAt(0)) ? str : StringUtils.reverse(str);
     }
 }
