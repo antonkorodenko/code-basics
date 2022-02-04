a24febe86bf83fc6921e7eb9a234adecb199f9ac
AntonK <antonkorodenko@gmail.com>
2022-02-04 20:46:12 +0300

    добавили задание 49 конструкция if-else

--- src/test/java/conditionalconstructions/Lesson48.java
+++ src/test/java/conditionalconstructions/Lesson48.java
@@ -1,11 +1,18 @@
 package conditionalconstructions;
-
 import org.junit.jupiter.api.Test;
 
 public class Lesson48 {
 
     @Test
     void test(){
+        System.out.println(normalizeUrl("google.com"));
+    }
 
+    public String normalizeUrl(String site) {
+        if (site.startsWith("https://")) {
+            return site;
+        }
+
+        return "https://" + site;
     }
 }
