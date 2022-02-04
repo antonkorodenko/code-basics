047f09b8064c791e897dc16a48e448d830f93d91
AntonK <antonkorodenko@gmail.com>
2022-02-04 23:11:17 +0300

    добавили задание 54 агрегация данных(числа)

--- src/test/java/cycles/Lesson54.java
+++ src/test/java/cycles/Lesson54.java
@@ -6,6 +6,19 @@ public class Lesson54 {
 
     @Test
     void test(){
+        System.out.println(multiplyNumbersFromRange(1, 5));
+    }
+    public static int multiplyNumbersFromRange(int start, int finish) {
+        // BEGIN (write your solution here)
+        var i = start;
+        var result = 1;
 
+        while (i <= finish) {
+            result = result * i;
+            i = i + 1;
+        }
+
+        return result;
+        // END
     }
 }
