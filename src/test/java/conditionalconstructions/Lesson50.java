0c07e3cdd4629598e1307f4e3f79ebb96ec44226
AntonK <antonkorodenko@gmail.com>
2022-02-04 20:58:30 +0300

    добавили задание 49 конструкция if-else

--- src/test/java/conditionalconstructions/Lesson50.java
+++ src/test/java/conditionalconstructions/Lesson50.java
@@ -6,6 +6,16 @@ public class Lesson50 {
 
     @Test
     void test(){
+        System.out.println(whoIsThisHouseToStarks("Karstark"));
 
+    }
+    public static String whoIsThisHouseToStarks(String houseName) {
+        if ("Karstark".equals(houseName) || "Tally".equals(houseName)) {
+            return "friend";
+        } else if ("Lannister".equals(houseName) || "Frey".equals(houseName)) {
+            return "enemy";
+        }
+
+        return "neutral";
     }
 }
