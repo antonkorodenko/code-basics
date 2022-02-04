721b74d023c29241104a902d5fe2b9a0f3e411a0
AntonK <antonkorodenko@gmail.com>
2022-02-04 22:55:54 +0300

    добавили задание 52 конструкция switch

--- src/test/java/conditionalconstructions/Lesson52.java
+++ src/test/java/conditionalconstructions/Lesson52.java
@@ -6,6 +6,19 @@ public class Lesson52 {
 
     @Test
     void test(){
+        System.out.println(getNumberExplanation(666));
 
+    }
+    public static String getNumberExplanation(int number) {
+        switch (number) {
+            case 666:
+                return "devil number";
+            case 7:
+                return "prime number";
+            case 42:
+                return "answer for everything";
+            default:
+                return null;
+        }
     }
 }
