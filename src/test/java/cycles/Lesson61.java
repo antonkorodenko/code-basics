a3456288fc79e4b6352d5d3b6b8bd1a1acc9df11
AntonK <antonkorodenko@gmail.com>
2022-02-05 18:40:24 +0300

    добавили задание 61 возврат из циклов

--- src/test/java/cycles/Lesson61.java
+++ src/test/java/cycles/Lesson61.java
@@ -6,6 +6,21 @@ public class Lesson61 {
 
     @Test
     void test(){
+        System.out.println(hasChar("Renly", 'R'));
 
+    }
+    public static boolean hasChar(String str, char ch) {
+        // BEGIN (write your solution here)
+        var i = 0;
+        while (i < str.length()) {
+            if (str.charAt(i) == ch) {
+                return true;
+            }
+
+            i += 1;
+        }
+
+        return false;
+        // END
     }
 }
