5e34ccbd794f5688ee863f68c27553c884921b3c
AntonK <antonkorodenko@gmail.com>
2022-02-05 18:43:27 +0300

    добавили задание 62 цикл for

--- src/test/java/cycles/Lesson62.java
+++ src/test/java/cycles/Lesson62.java
@@ -6,6 +6,18 @@ public class Lesson62 {
 
     @Test
     void test(){
+        System.out.println(encrypt("move"));
 
+    }
+    public static String encrypt(String str) {
+        // BEGIN (write your solution here)
+        var result = "";
+        for (var i = 0; i < str.length(); i += 2) {
+            var nextSymbol = (i + 2 > str.length()) ? "" : str.substring(i + 1, i + 2);
+            result = result + nextSymbol + str.charAt(i);
+        }
+
+        return result;
+        // END
     }
 }
