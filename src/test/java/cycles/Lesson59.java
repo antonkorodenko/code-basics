779f9d025853a9e9be011c6b6433ca7fb51f71fc
AntonK <antonkorodenko@gmail.com>
2022-02-05 18:31:00 +0300

    добавили задание 58 синтаксический сахар

--- src/test/java/cycles/Lesson59.java
+++ src/test/java/cycles/Lesson59.java
@@ -6,6 +6,23 @@ public class Lesson59 {
 
     @Test
     void test(){
+        var str = "If I look back I am lost";
+        System.out.println(filterString(str, 'I'));
 
+    }
+    public static String filterString(String str, char ch) {
+        // BEGIN (write your solution here)
+        var i = 0;
+        var result = "";
+        while (i < str.length()) {
+            var currentChar = str.charAt(i);
+            if (currentChar != ch) {
+                result = result + currentChar;
+            }
+            i += 1;
+        }
+
+        return result;
+        // END
     }
 }
