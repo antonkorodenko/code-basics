3d21bf682c9c62430cd09385b798cc4054f56e2e
AntonK <antonkorodenko@gmail.com>
2022-02-05 18:36:53 +0300

    добавили задание 60 инкремент и декремент

--- src/test/java/cycles/Lesson60.java
+++ src/test/java/cycles/Lesson60.java
@@ -7,5 +7,24 @@ public class Lesson60 {
     @Test
     void test(){
 
+        var text = "I never look back";
+        System.out.println(makeItFunny(text, 3));
+    }
+    public static String makeItFunny(String str, int n) {
+        // BEGIN (write your solution here)
+        var i = 0;
+        var result = "";
+        while (i < str.length()) {
+            var current = str.charAt(i);
+            if ((i + 1) % n == 0) {
+                result = result + Character.toUpperCase(current);
+            } else {
+                result = result + current;
+            }
+            i++;
+        }
+
+        return result;
+        // END
     }
 }
